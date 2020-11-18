b3fad0b737052911d4cb7d0d4cddd86b0a70f5f9
sayaka <salyaka623@gamil.com>
2020-11-18 18:05:49 +0900

    Q10 修正

--- task.rb
+++ task.rb
@@ -89,10 +89,12 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-  if foods.include?("うに")
-    puts "好物です"
-  else
-    puts "まぁまぁ好きです"
+  foods.each do |food|
+    if food.include?("うに")
+      puts "好物です"
+    else
+      puts "まぁまぁ好きです"
+     end
   end
   
 end
@@ -101,7 +103,7 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+  
 end
 
 def q12
